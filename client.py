4271e193e7155921519f9c021b06f8fd06462ae6
Абдуллина Дария Сергеевна <181416@edu.fa.ru>
2019-12-27 09:52:08 +0300

    Ассимметричный шифр

--- client.py
+++ client.py
@@ -1,14 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 08:44:13 2019
+
+@author: 181416
+"""
+#ALICE
 import socket
 import pickle
 
+def shifr(msg,k):
+    rez=''
+    for i in range(len(msg)):
+        rez+=chr(ord(msg[i])^k)
+    return rez
+    
+def send_to_server(conn,msg,k):
+    conn.send(pickle.dumps(shifr(msg,k)))
+    
+def receive_from_server(conn,k):
+    msg=shifr(pickle.loads(conn.recv(1024)), k)
+    return msg
+    
+
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8083
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
-
 p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
+A = g**a%p #генерируем A
+print(A)
+sock.send(pickle.dumps((p, g, A))) #отправляем на сервер
+B=pickle.loads(sock.recv(1024))
+K=B**a%p #генерируем K
+print("K:", K)
+msg='Hello!'
+send_to_server(sock, msg, K) #отправляем клиенту K
+print(' ' + receive_from_server(sock, K))
 sock.close()
+
+
+
--- server.py
+++ server.py
@@ -1,15 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 08:45:00 2019
+
+@author: 181416
+"""
+#Bob
 import socket
 import pickle
 
+def shifr(msg,k):
+    rez=''
+    for i in range(len(msg)):
+        rez+=chr(ord(msg[i])^k)
+    return rez   
+
+def send_to_client(conn,msg,k):
+    conn.send(pickle.dumps(shifr(msg,k)))
+    
+def receive_from_client(conn,k):
+    msg=shifr(pickle.loads(conn.recv(1024)), k)
+    return msg
+    
 HOST = '127.0.0.1'
-PORT = 8080
+PORT = 8083
 
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
-
+b = 2 #секретное число b
 msg = conn.recv(1024)
-print(pickle.loads(msg))
-
+p, g, A = pickle.loads(msg)
+B = g**b%p #вычисляем B
+print(B)
+conn.send(pickle.dumps(B)) #отправляем клиенту
+K = A**b%p #генерируем К
+print("K:", K)
+msg = receive_from_client(conn, K) #получаем число от клиента
+print(msg)
+send_to_client(conn, K, 'Собщение расшифровано')
 conn.close()
+
+
